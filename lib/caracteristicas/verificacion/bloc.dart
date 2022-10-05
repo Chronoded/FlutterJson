deedc9bc0eb3d8626193515ce6cb10c7cb15c8ad
oliverAcosta <oli_olli@hotmail.com>
2022-10-04 21:36:42 -0600

    Estados y Eventos Creado, agregue dependencia Bloc

---
+++ lib/caracteristicas/verificacion/bloc.dart
@@ -0,0 +1,24 @@
+//Estados y Eventos
+
+class Estado {}
+
+class Creandose extends Estado{}
+
+class SolicitarRaza extends Estado {}
+
+class EsperandoConfirmacion extends Estado {}
+
+class MostrandoRazaConf extends Estado  {}
+
+class MostrandoRazaNoConf extends Estado{}
+
+
+class Evento {}
+
+class Creado extends Evento{}
+
+class NombreRecibidoRaza extends Evento{}
+
+class Verificacion extends Evento {}
+
+
